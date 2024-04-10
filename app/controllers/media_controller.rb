6a6a72f973826967eca8e675aa937bb077dc02e5
mickeymarse <mmserrapica@tutanota.com>
2024-04-10 16:11:40 +0100

    Created Medium model, db schema and trialed MVC.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,4 +1,5 @@
 class MediaController < ApplicationController
   def index
+    @media = Medium.all
   end
 end
--- app/graphql/types/medium_type.rb
+++ app/graphql/types/medium_type.rb
@@ -2,10 +2,10 @@
 
 module Types
   class MediumType < Types::BaseObject
+    field :id, ID, null: false
     field :title, String
-    field :type, String
-    field :rating, Float
-    field :genre, String
     field :review, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
