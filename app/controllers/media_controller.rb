88d71ba964ce027111e4b6533fd107f76938b336
mickeymarse <mmserrapica@tutanota.com>
2024-04-26 11:32:44 +0100

    Created initial id routing for individual entry

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,4 +2,8 @@ class MediaController < ApplicationController
   def index
     @media = Medium.all
   end
+
+  def show
+    @medium = Medium.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "media#index"
   get "/media", to: "media#index"
+  get "/media/:id", to: "media#show"
 end
