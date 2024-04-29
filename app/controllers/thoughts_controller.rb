29ce9488cee534f22245c9decd5c9faf658509f8
mickeymarse <mmserrapica@tutanota.com>
2024-04-29 11:54:40 +0100

    added thoughts deletion

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,8 +1,15 @@
 class ThoughtsController < ApplicationController
-     def create
+  def create
     @medium = Medium.find(params[:medium_id])
     @thought = @medium.thoughts.create(thought_params)
     redirect_to medium_path(@medium)
+  end
+
+  def destroy
+    @medium = Medium.find(params[:medium_id])
+    @thought = @medium.thoughts.find(params[:id])
+    @thought.destroy
+    redirect_to medium_path(@medium), status: :see_other
   end
 
   private
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,7 +1,7 @@
 class Medium < ApplicationRecord
     include Visible
     
-    has_many :thoughts
+    has_many :thoughts, dependent: :destroy
     
     validates :title, presence: true
     validates :review, presence: true, length: { minimum: 7 }
